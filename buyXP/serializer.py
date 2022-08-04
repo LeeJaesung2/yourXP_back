b8f8bcd6886c2b3cc6ea98ed1d1de4ece8f983aa
ganggnagme <kga0972@gmail.com>
2022-08-04 13:55:38 +0900

    model 수정

--- buyXP/serializer.py
+++ buyXP/serializer.py
@@ -4,9 +4,9 @@ from .models import Buy, Writer
 class WriterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Writer
-        fields = ('id', 'title', 'body')
+        fields = ('id', 'title', 'body', 'due_date', 'point', 'pub_date')
 
 class BuySerializer(serializers.ModelSerializer):
     class Meta:
         model = Buy
-        fields = ('id', 'buy_id', 'author', 'due_date', 'point', 'pub_date')
+        fields = ('id', 'buy_id', 'author', 'pub_date')
