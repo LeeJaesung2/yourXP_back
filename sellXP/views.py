bc4df3e209f725cba306bd55423344e0f6d4b123
wodnrP <wogajang22@naver.com>
2022-08-05 01:39:41 +0900

    sellXP model test

--- sellXP/views.py
+++ sellXP/views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializer import PostsSerializer
-from .models import Posts, TestModel
+from .models import Posts
 # Create your views here.
 
 @api_view(['GET'])
