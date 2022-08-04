c1a00ddf8483f41e064cf4db4b1365584b154296
kimminji000 <minji6764@naver.com>
2022-08-04 16:34:34 +0900

    user model

---
+++ user/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    real_name = models.CharField(max_length=100, null=False)
+    profile = models.ImageField(null=True)
+    nickname = models.CharField(max_length=100, unique = True)
+    email = models.EmailField(max_length=100, unique = True)
+    point = models.IntegerField(default=0)
