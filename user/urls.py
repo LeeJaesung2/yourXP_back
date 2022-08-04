6d7b7d9b77b28f89fa2fad9a4f5f311889824408
kimminji000 <minji6764@naver.com>
2022-08-04 17:32:33 +0900

    modify user urls

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('users', views.getUsers, name="getUsers"),
-    path('user', views.createUser, name="createUser"),
-    path('user/<user_id>', views.userDetail, name="readUpdateDelete"),
+    path('all', views.getUsers, name="getUsers"),
+    path('', views.createUser, name="createUser"),
+    path('<user_id>', views.userDetail, name="readUpdateDelete"),
 ]
