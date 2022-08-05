08ebf9bd2c8053110fb73d4f952cd07ff92606d9
wodnrP <wogajang22@naver.com>
2022-08-05 10:04:10 +0900

    model

--- sellXP/models.py
+++ sellXP/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-class TestModel(models.Model):
+class SellXP_tag(models.Model):
     pass
 
 # Create your models here.
-class Posts(models.Model):
-    s_id = models.BigAutoField(primary_key=True)
+class SellXP(models.Model):
+    #s_id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=200)
     text = models.TextField()
     create_time = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(TestModel, related_name='+', on_delete=models.CASCADE, default="", blank=True, null=True)
+    user = models.ForeignKey(User, related_name='+', on_delete=models.CASCADE, default="", blank=True, null=True)
     hits = models.PositiveIntegerField(default=0)
     recommend = models.PositiveIntegerField(default=0)
     price = models.IntegerField()
-    sellXP_tag = models.ForeignKey(TestModel, related_name='+', on_delete=models.CASCADE, default="", blank=True, null=True)
+    sellXP_tag = models.ForeignKey(SellXP_tag, related_name='+', on_delete=models.CASCADE, default="", blank=True, null=True)
 
