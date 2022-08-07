0dbe79b1e63f9acc5b92b1c19a6a0ab3364bddee
LeeJaesung2 <dlwotjd9909@naver.com>
2022-08-07 14:25:54 +0900

    수정

--- buyXP/urls.py
+++ buyXP/urls.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    
-]
-=======
 from django.contrib import admin
 from django.urls import path
 from buyXP import views
@@ -16,5 +8,3 @@ urlpatterns = [
     path('buy', views.createBuyXP, name="createBuyXP"),
     path('buys/update/<id>', views.updateBuyXP, name="updateBuyXP"),
 ]
-
->>>>>>> c2a8adb83ca1f428e054c54f1acdca866ec8f31d
