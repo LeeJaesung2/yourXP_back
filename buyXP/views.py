31eb2eba6631f5543a3a50a55a16a04dbbb67be3
ganggnagme <kga0972@gmail.com>
2022-08-07 14:02:12 +0900

    주석 및 tag 추가

--- buyXP/views.py
+++ buyXP/views.py
@@ -11,7 +11,7 @@ from buyXP import serializer
 
 # Create your views here.
 
-#불러오기 BuyXP와 BuyXP_tag 동시에 불러오는 것으로 수정하는 중 (현재는 BuyXP만 불러오기 함)
+#불러오기 BuyXP와 BuyXP_tag 동시에 불러오는 것으로 수정하는 중 BuyXP_tag도 같이 로딩되도록 모두 수정함
 @api_view(['GET'])
 def getBuyXP(request):
     buys = BuyXP.objects.all()
@@ -20,7 +20,7 @@ def getBuyXP(request):
     tagsSerializer = BuyXP_tagSerializer(tags, many=True)
     return Response(buysSerializer.data), Response(tagsSerializer.data)
 
-
+#디테일
 @api_view(['GET'])
 def detailBuyXP(request):
     buys = BuyXP.objects.get(pk=id)
@@ -32,22 +32,26 @@ def detailBuyXP(request):
         return Response(buysSerializer.data, tagsSerializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
+#크리에이트
 @api_view(['POST'])
 def createBuyXP(request):
-    serializer = BuyXPSerializer(data=request.data)
+    buysSerializer = BuyXPSerializer(data=request.data)
+    tagsSerializer = BuyXP_tagSerializer(data=request.data)
+    serializer = buysSerializer, tagsSerializer
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status = status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-
+# Update
 @api_view(['PATCH'])
 def updateBuyXP(request):
     print(request.data)
     buys = BuyXP.objects.get(pk=id) 
-    serializer = BuyXPSerializer(buys, data=request.data, partial=True)
-    
+    tags = BuyXP_tag.objects.get(pk=id)
+    buysSerializer = BuyXPSerializer(buys, data=request.data, partial=True)
+    tagsSerializer = BuyXP_tagSerializer(tags, data=request.data, partial=True)
+    serializer = buysSerializer, tagsSerializer
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status = status.HTTP_201_CREATED)
@@ -58,4 +62,6 @@ def updateBuyXP(request):
 def deleteBuyXP(request):
     buys = BuyXP.objects.get(pk=id)
     buys.delete()
+    tags = BuyXP_tag.objects.get(pk=id)
+    tags.delete()
     return Response({'message':'success', 'code':'200'})
