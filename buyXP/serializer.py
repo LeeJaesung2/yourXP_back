27def900b3430c2f37fa3dd8e6f35161600378ba
ganggnagme <kga0972@gmail.com>
2022-08-07 14:24:09 +0900

    hit null=true 삽입

--- buyXP/serializer.py
+++ buyXP/serializer.py
@@ -9,4 +9,4 @@ class BuyXPSerializer(serializers.ModelSerializer):
 class BuyXP_tagSerializer(serializers.ModelSerializer):
     class Meta:
         model = BuyXP_tag
-        fields = ('tag_id','tag1', 'tag2','tag3','tag4','tag5','tag6','tag7','tag8') 
+        fields = ('tag1', 'tag2','tag3','tag4','tag5','tag6','tag7','tag8') 
