e6c5abfe0cfb9ecf3234389b9fa137ad0b4a4389
LeeJaesung2 <dlwotjd9909@naver.com>
2022-08-08 10:44:48 +0900

    setting 수정

--- yourXP/settings.py
+++ yourXP/settings.py
@@ -41,7 +41,7 @@ SECURE_SSL_REDIRECT = True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'cloudtype.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.cloudtype.app']
 
 AUTH_USER_MODEL = 'user.User'
 
