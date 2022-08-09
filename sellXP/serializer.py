3eeb1a0aea280cbc7863c18be331c899b92c12ea
kimminji000 <minji6764@naver.com>
2022-08-09 14:48:52 +0900

    review api modified

--- sellXP/serializer.py
+++ sellXP/serializer.py
@@ -6,7 +6,7 @@ from dataclasses import field
 class SellXPSerializer(serializers.ModelSerializer):
     class Meta:
         model = SellXP
-        fields = ('id', 'title', 'text', 'hits', 'recommend', 'price')
+        fields = ('title', 'text', 'hits', 'recommend', 'price')
 
 class Sell_reviewSerializer(serializers.ModelSerializer):
     class Meta:
