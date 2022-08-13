77cb296e08e7cb4bab7e7c82998a1ebe050da8a1
kimminji000 <minji6764@naver.com>
2022-08-13 20:42:34 +0900

    point

--- sellXP/admin.py
+++ sellXP/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import SellXP, SellXP_tag
-from .models import Sell_review
+from .models import SellXP, SellXP_tag, Sell_review, Sell_image
 
 # Register your models here.
 admin.site.register(SellXP)
 admin.site.register(SellXP_tag)
 admin.site.register(Sell_review)
+admin.site.register(Sell_image)
