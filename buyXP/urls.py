d0a45451030cc238eea1c43147b9e611b8c5276b
LeeJaesung2 <dlwotjd9909@naver.com>
2022-08-15 01:42:02 +0900

    주석 추가

--- buyXP/urls.py
+++ buyXP/urls.py
@@ -1,17 +1,7 @@
-from django.contrib import admin
 from django.urls import path
-from yourXP import views
 from .import views
-from buyXP import views
-#후
-URLPatterns = [
-    path('admin', admin.site.urls),
-    path('buys', views.getBuyById, name="getBuys"),
-    path('buy', views.createBuy, name="createBuy"),
-    path('buy/<buy_id>', views.getBuyById, name="getReviewById"),
-    path('buy/<buy_id>', views.updateBuy, name="updateReview"),
-    path('buy/<buy_id>', views.deleteBuy, name="deleteReview"),
-#전
+
+urlpatterns = [
     path('buys', views.getBuyXP, name="getBuyXP"),
     path('buys/detail/<id>', views.detailBuyXP, name="detailBuyXP"),
     path('buy', views.createBuyXP, name="createBuyXP"),
