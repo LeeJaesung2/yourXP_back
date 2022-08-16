5ec8ce350d37eea394afc200db6a6b5cb32d72cc
wodnrP <wogajang22@naver.com>
2022-08-16 15:17:43 +0900

    sellXP tag crud 생성

--- buyXP/urls.py
+++ buyXP/urls.py
@@ -1,16 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from yourXP import views
 from .import views
-from buyXP import views
 #후
-URLPatterns = [
+urlpatterns = [
     path('admin', admin.site.urls),
-    path('buys', views.getBuyById, name="getBuys"),
-    path('buy', views.createBuy, name="createBuy"),
-    path('buy/<buy_id>', views.getBuyById, name="getReviewById"),
-    path('buy/<buy_id>', views.updateBuy, name="updateReview"),
-    path('buy/<buy_id>', views.deleteBuy, name="deleteReview"),
+    #path('buys', views.getBuyById, name="getBuys"),
+    #path('buy', views.createBuy, name="createBuy"),
+    #path('buy/<buy_id>', views.getBuyById, name="getReviewById"),
+    #path('buy/<buy_id>', views.updateBuy, name="updateReview"),
+    #path('buy/<buy_id>', views.deleteBuy, name="deleteReview"),
 #전
     path('buys', views.getBuyXP, name="getBuyXP"),
     path('buys/detail/<id>', views.detailBuyXP, name="detailBuyXP"),
--- sellXP/urls.py
+++ sellXP/urls.py
@@ -6,13 +6,13 @@ from django.conf import settings
 from sellXP.views import sellXPViewSet
 
 router = DefaultRouter()
-router.register('create', sellXPViewSet)
+router.register(r'create', views.sellXPViewSet)
 
 
 urlpatterns = [
     path('', views.getSellXPs, name="getSellXPs"),    
     path("", include(router.urls)),
-    #path('create', views.createSellXP, name="createSellXP"),
+#   path('create', views.createSellXP, name="createSellXP"),
     path('<sellXP_id>', views.getSellXP, name="getSellXP"),
     path('update/<sellxp_id>', views.updateSellXP, name="updateSellXP"),
     path('delete/<int:sellxp_id>', views.deleteSellXP, name="deleteSellXP"),
@@ -20,4 +20,9 @@ urlpatterns = [
     path('<sellXP_id>/review', views.createReview, name="createReview"),
     path('<sellXP_id>/review/<sell_review_id>', views.reviewDetail, name="readUpdateDelete"),
     path('like/<int:sellxp_id', views.sellXP_like, name="sellXP_like"),
+
+    path('tag/<sellXPtag_id>', views.getSellXP_tag, name="getSellXP_tag"),
+    path('tag_create', views.createSellXP_tag, name="createSellXP_tag"),
+    path('tag_update/<sellxptag_id>', views.updateSellXP_tag, name="updateSellXP_tag"),
+    path('tag_delete/<int:sellxptag_id>', views.deleteSellXP_tag, name="deleteSellXP_tag"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
