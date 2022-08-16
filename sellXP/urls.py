079754118b0b0e4953a0174702c1e0cbba4cefb4
LeeJaesung2 <dlwotjd9909@naver.com>
2022-08-16 16:34:20 +0900

    오타 수정

--- sellXP/urls.py
+++ sellXP/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('<sellXP_id>/reviews', views.getReviews, name="getReviews"),
     path('<sellXP_id>/review', views.createReview, name="createReview"),
     path('<sellXP_id>/review/<sell_review_id>', views.reviewDetail, name="readUpdateDelete"),
-    path('like/<int:sellxp_id', views.sellXP_like, name="sellXP_like"),
+    path('like/<int:sellxp_id>', views.sellXP_like, name="sellXP_like"),
 
     path('tag/<sellXPtag_id>', views.getSellXP_tag, name="getSellXP_tag"),
     path('tag_create/<sellXPtag_id>', views.createSellXP_tag, name="createSellXP_tag"),
