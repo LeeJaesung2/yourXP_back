203a29e39935e7a7d3f401d07897e402e0bc5176
wodnrP <wogajang22@naver.com>
2022-08-19 01:15:45 +0900

    sellXP id값 생성

--- sellXP/serializer.py
+++ sellXP/serializer.py
@@ -10,7 +10,7 @@ class SellXPSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = SellXP
-        fields = ('title','text','create_time','user','hits','recommend','price', 'images','tag1','tag2','tag3','tag4','tag5','tag6','tag7','tag8','tag9','tag10')
+        fields = ('id','title','text','create_time','user','hits','recommend','price', 'images','tag1','tag2','tag3','tag4','tag5','tag6','tag7','tag8','tag9','tag10')
 
     def create(self, validated_data):
         instance  = SellXP.objects.create(**validated_data)
